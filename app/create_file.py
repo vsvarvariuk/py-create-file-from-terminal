9b890d1279566c72ed4068834652eb0b89b7ffcb
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-18 14:34:36 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,61 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+def create_directory(path_parts):
+    path = os.path.join(*path_parts)
+    os.makedirs(path, exist_ok=True)
+    print(f"Created directory: {path}")
+
+
+def create_file(file_1):
+    content = []
+    while True:
+        input_info = input("enter content:")
+        if input_info.lower() == "stop":
+            break
+        content.append(input_info)
+    data_now = datetime.datetime.now()
+    first_str = data_now.strftime("%Y-%m-%d %H:%M:%S")
+
+    if os.path.exists(file_1):
+        with open(file_1, "a") as f:
+            f.write(f"\n{first_str}\n")
+            for num, elem in enumerate(content, start=1):
+                f.write(f"{num} {elem}\n")
+
+    else:
+        with open(file_1, "w") as f:
+            f.write(f"{first_str}\n")
+            for num, elem in enumerate(content, start=1):
+                f.write(f"{num} {elem}\n")
+
+
+def main():
+    args = sys.argv[1:]
+    dir_parts = []
+    name_file = None
+    for arg in args:
+        if arg == "-d":
+            for i in args[args.index(arg) + 1 :]:
+                if i != "-f":
+                    dir_parts.append(i)
+                else:
+                    break
+        elif arg == "-f":
+            name_file = args[args.index(arg) + 1:]
+            break
+
+    if dir_parts:
+        create_directory(dir_parts)
+    if name_file:
+        if dir_parts:
+            file_path = os.path.join(*dir_parts, name_file[0])
+        else:
+            file_path = name_file[0]
+        create_file(file_path)
+
+
+if __name__ == "__main__":
+    main()
